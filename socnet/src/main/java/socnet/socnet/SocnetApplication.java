98c4c18e60af1ba62fbed875243a7e3382af5e3c
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-09-17 15:50:30 +0500

    test home page

---
+++ socnet/src/main/java/socnet/socnet/SocnetApplication.java
@@ -0,0 +1,15 @@
+package socnet.socnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocnetApplication.class, args);
+	}
+
+}
+
+
