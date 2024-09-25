b71484588f35af65103b55fd17ad5e33fa1f9880
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-09-25 20:08:18 +0500

    add interfaceSetttings class

--- socnet/src/main/java/socnet/socnet/Controller/MainController.java
+++ socnet/src/main/java/socnet/socnet/Controller/MainController.java
@@ -1,4 +1,4 @@
-package socnet.socnet;
+package socnet.socnet.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController{
 
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String home(){
-		return "home.html";
+		return "index.html";
 	}
 
 }
