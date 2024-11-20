f3fffb0ac62afb07696cbf1f65d2dc818efc7e4d
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-11-20 22:35:55 +0500

    add controller

---
+++ socnet/src/main/java/socnet/socnet/RestController/PublicationController.java
@@ -0,0 +1,12 @@
+package socnet.socnet.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/publication")
+public class PublicationController {
+
+    
+
+}
